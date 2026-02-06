f0de7172113bf5188d7692b7a0ad7aba4cce27fb
Anmol Vishisht <anmolvishisht45@gmail.com>
2026-02-06 22:36:03 +0530

    KeyInputs

--- script.js
+++ script.js
@@ -1,13 +1,92 @@
+
+
 const board = document.querySelector(".board");
-const blockHeight = 30;
-const blockWidth = 30;
+const blockHeight = 50;
+const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth/blockWidth);
 const rows = Math.floor(board.clientHeight/blockHeight);
 
-for (let index = 0; index < rows*cols; index++) {
-    const block = document.createElement("div");
-    block.classList.add("block");
-    board.appendChild(block);
-    
+const blocks = [];
+const snake = [{
+    x:1 , y:3
+},{
+    x:1 , y:4
+},{
+    x:1 , y:5
+}]
+
+let direction = "down";
+let intervalId = null;
+
+for (let row = 0 ; row < rows; row++){
+    for(let col = 0 ; col < cols ; col++){
+        const block = document.createElement("div");
+        block.classList.add("block");
+        board.appendChild(block);
+        block.innerHTML = `${row}-${col}`
+        blocks[`${row}-${col}`] = block;
+    }
 }
+
+
+function render(){
+    snake.forEach(segment => {
+        
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    });
+}
+
+function clearSnake() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`]?.classList.remove("fill");
+    });
+}
+
+render();
+
+intervalId = setInterval(()=>{
+
+    let head = null;
+    if(direction==="left"){
+        head = { x: snake[0].x , y: snake[0].y-1}
+    }
+    else if(direction==="right"){
+        head = { x: snake[0].x , y: snake[0].y+1}
+    }
+    else if(direction==="down"){
+        head = { x: snake[0].x+1 , y: snake[0].y}
+    }
+    else if(direction==="up"){
+        head = { x: snake[0].x-1 , y: snake[0].y}
+    }
+
+    if(head.x < 0 || head.x >=rows || head.y < 0 || head.y >= cols ){
+        alert("Game over");
+        clearInterval(intervalId)
+    }
+    
+    clearSnake();
+    snake.unshift(head)
+    snake.pop()
+    render();
+
+},300);
+
+
+
+addEventListener("keydown",(event)=>{
+    
+    if(event.key=="ArrowUp"){
+        direction = "up";
+    }
+    else if(event.key=="ArrowDown"){
+        direction = "down";
+    }
+    else if(event.key=="ArrowLeft"){
+        direction = "left";
+    }
+    else if(event.key=="ArrowRight"){
+        direction = "right";
+    }
+});

706e9f8d8e28a5ffeb0d7c1860dc5e93bff3f762
Anmol Vishisht <anmolvishisht45@gmail.com>
2026-02-07 01:14:54 +0530

    Time

--- script.js
+++ script.js
@@ -31,13 +31,14 @@ let food = {x:Math.floor(Math.random()*rows), y:Math.floor(Math.random()*cols)};
 
 let direction = "down";
 let intervalId = null;
+let timeInterval = null;
 
 for (let row = 0 ; row < rows; row++){
     for(let col = 0 ; col < cols ; col++){
         const block = document.createElement("div");
         block.classList.add("block");
         board.appendChild(block);
-        block.innerHTML = `${row}-${col}`
+        //block.innerHTML = `${row}-${col}`
         blocks[`${row}-${col}`] = block;
     }
 }
@@ -45,6 +46,7 @@ for (let row = 0 ; row < rows; row++){
 
 function render(){
     let head = null;
+    
     blocks[`${food.x}-${food.y}`].classList.add("food");
     if(direction==="left"){
         head = { x: snake[0].x , y: snake[0].y-1}
@@ -131,20 +133,26 @@ startBtn.addEventListener("click",()=>{
     intervalId = null;
     modal.style.display = "none";
     intervalId = setInterval(()=>{render();},200);
+    timeInterval = setInterval(()=>{
+        let[min,sec] = time.split("-").map(Number);
+
+        if(sec==59){
+            min +=1;
+            sec = 0;
+        }
+        else{
+            sec +=1;
+        }
+
+        time = `${min}-${sec}`
+        timeElement.innerText = time;
+    },1000)
 })
 
 addEventListener("keydown",(event)=>{
     
-    if(event.key=="ArrowUp"){
-        direction = "up";
-    }
-    else if(event.key=="ArrowDown"){
-        direction = "down";
-    }
-    else if(event.key=="ArrowLeft"){
-        direction = "left";
-    }
-    else if(event.key=="ArrowRight"){
-        direction = "right";
-    }
+    if (event.key === "ArrowUp" && direction !== "down") direction = "up";
+    else if (event.key === "ArrowDown" && direction !== "up") direction = "down";
+    else if (event.key === "ArrowLeft" && direction !== "right") direction = "left";
+    else if (event.key === "ArrowRight" && direction !== "left") direction = "right";
 });
